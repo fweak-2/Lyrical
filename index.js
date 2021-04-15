57e2a19b3f6e4358efcd35dcc473c1510f34b425
spin~ <spinfal@atheist.com>
2021-04-15 03:13:07 -0700

    Command to getlyrics and sendlyrics

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const Discord = require('discord.js');
 const fs = require('fs');
+const GeniusFetcher = require('genius-lyrics-fetcher');
+
+const ACCESS_TOKEN = '7PPBgn5n4hkgph58yQQNa-3GNgTLI3mzIV8x6IRIME4wFOn-flZ97zt6VroVIjdd';
+const gfetch = new GeniusFetcher.Client(ACCESS_TOKEN);
 
 const client = new Discord.Client();
 const lyrics = fs.readFileSync('lyrics.txt', 'utf-8').replace(/\r/g, '').split('\n');
@@ -9,11 +13,40 @@ client.on('ready', () => {
     console.log(`Started Lyrical -> ${client.user.tag}`);
 });
 
-client.on('message', async (message) => {
+client.on('message', async(message) => {
     if (message.author.bot) return;
     if (message.author.id !== config.yourId) return console.log('Author Check');
 
     if (message.content === 'hey') return message.reply('\n> Hello');
+
+    if (message.content.startsWith('getlyrics')) {
+        function writeToFile(newLyrics) {
+            fs.writeFile('lyrics.txt', newLyrics.toString(), function (err) {
+                if (err) return console.log(err);
+                console.log('lyrics saved');
+            });
+        }
+        let discordLyrics = message.content.replace('getlyrics ', '');
+        if (message.content.includes('/') === false) {
+            message.channel.send('Correct format: `getlyrics ' + discordLyrics + ' / Artist Name`');
+            return;
+        }
+        discordLyrics = discordLyrics.split('/');
+        gfetch.fetch(discordLyrics[0], discordLyrics[1])
+            .then(result => writeToFile(result.lyrics));
+    }
+
+    if (message.content.startsWith('sendlyrics')) {
+        fs.readFile('lyrics.txt', 'utf8', function (err,data) {
+            if (err) {
+                return console.log(err);
+            }
+            if (data.length > 2000) {
+                return message.channel.send('I cannot send the lyrics as it\'s more than 2000 characters!');
+            }
+            return message.channel.send(data);
+        });
+    }
 
     if (message.content === 'lyrical') {
         await message.delete();
